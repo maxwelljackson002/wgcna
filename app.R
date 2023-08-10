b56c2bdea4ba95b1ed2c471e5ed2909e9a7cee71
maxwelljackson002 <116917925+maxwelljackson002@users.noreply.github.com>
2023-08-10 12:01:28 -0500

    Sliders added, not fully functional

--- app.R
+++ app.R
@@ -4,6 +4,9 @@ library(WGCNA)
 library(igraph)
 library(BiocManager)
 library(tidyverse)
+library(shinyjs)
+library(gplots)
+
 options(repos = BiocManager::repositories())
 
 # Define UI for application that draws a histogram
@@ -16,11 +19,10 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput("dataFile", "Upload Gene Expression Data", accept = c(".csv", ".xlsx")),
-#      sliderInput("bins",
- #                 "Number of bins:",
-  #                min = 1,
-   #               max = 2700,
-    #              value = 30),
+      sliderInput("numPointsDendrogram", "Number of Points (Dendrogram):", min = 1, max = 100, value = 30),
+      sliderInput("numPointsNetwork", "Number of Points (Network):", min = 1, max = 100, value = 30),
+      sliderInput("numPointsHeatmap", "Number of Points (Heatmap):", min = 1, max = 100, value = 30),
+      actionButton("resetButton", "Reset")
     ),
     
     # Show a plot of the generated distribution
@@ -29,14 +31,14 @@ ui <- fluidPage(
         tabPanel("Module Assignment", dataTableOutput("moduleTable")),
         tabPanel("Module Dendrogram", plotOutput("moduleDendrogram")),
         tabPanel("Module Network", plotOutput("moduleNetwork")),
-#       tabPanel("Module Detection", plotOutput("moduleDetectionTable"))
+        tabPanel("Module Heatmap", plotOutput("heatmapPlot"))
       )
     )
   )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   # Process the uploaded file and display as a table
   output$moduleTable <- renderDataTable({
@@ -50,18 +52,25 @@ server <- function(input, output) {
     }
   })
   
-  # Generate a dendrogram from the uploaded data file
+  # Generate a dendrogram from the uploaded data file with variable number of points
   output$moduleDendrogram <- renderPlot({
     # Check if a file is uploaded
     if (!is.null(input$dataFile)) {
       # Read the uploaded file
       df <- read.xlsx(input$dataFile$datapath)
-      
-      # Generate dendrogram
-      dendro <- hclust(dist(df))
-      
-      # Plot the dendrogram
-      plot(dendro, main = "Gene Co-expression Dendrogram")
+
+      # Retrieve the number of points to display from the sliderInput
+      num_points <- input$numPointsDendrogram
+
+      # Generate dendrogram with the specified number of points
+      dendrogram <- hclust(dist(df))
+
+      # Cut the dendrogram into the desired number of clusters based on the number of points selected by the user
+      dendrogram_cut <- cutree(dendrogram, k = num_points)
+
+      # Plot the dendrogram with limited points
+      plot(dendrogram, main = "Gene Co-expression Dendrogram with Limited Points")
+      rect.hclust(dendrogram, k = num_points, border = 2)
     }
   })
   
@@ -72,8 +81,10 @@ server <- function(input, output) {
       # Read the uploaded file
       df <- read.xlsx(input$dataFile$datapath)
       
-      # Perform network analysis and generate the module network plot
+      # Retrieve the number of points to display from the sliderInput
+      num_points <- input$numPointsNetwork
       
+      # Perform network analysis and generate the module network plot
       # Convert the data frame to a numeric matrix
       datExpr <- as.matrix(df)
       
@@ -99,28 +110,50 @@ server <- function(input, output) {
     }
   })
   
-  # # Generate module assignments for the genes
-  # output$moduleTable <- renderDataTable({
-  #   # Check if a file is uploaded
-  #   if (!is.null(input$dataFile)) {
-  #     # Read the uploaded file
-  #     df <- read.xlsx(input$dataFile$datapath)
-  # 
-  #     # Perform module detection using WGCNA
-  #     exprsData <- as.data.frame(t(df))
-  #     datExpr <- as.data.frame(exprsData)  # Convert to data frame
-  #     geneTree <- hclust(dist(t(df)))
-  #     modules <- cutreeDynamic(geneTree, method = "hybrid", deepSplit = 2)
-  # 
-  #     # Add module assignments to the data frame
-  #     datExpr$Module <- modules
-  # 
-  #     # Return the data frame with module assignments
-  #     datExpr
-  #   }
-  # })
+  # Generate a heatmap from the uploaded data file
+  output$heatmapPlot <- renderPlot({
+    # Check if a file is uploaded
+    if (!is.null(input$dataFile)) {
+      # Read the uploaded file
+      df <- read.xlsx(input$dataFile$datapath)
+      
+      # Convert non-numeric values to NA in the entire data frame
+      df[] <- lapply(df, as.numeric)
+      
+      # Retrieve the number of points to display from the sliderInput
+      num_points <- input$numPointsHeatmap
+      
+      # Generate the heatmap using the heatmap.2 function from gplots
+      heatmap.2(as.matrix(df), Rowv = NA, Colv = NA, scale = "row",
+                trace = "none", col = rev(heat.colors(256)))
+    }
+  })
 }
+
+# Reactive value to store the uploaded data
+uploaded_data <- reactiveVal(NULL)
+
+# Function to reset the app's state
+observeEvent(input$resetButton, {
+  uploaded_data(NULL)
+  output$moduleDendrogram <- NULL
+  output$moduleNetwork <- NULL
+  output$heatmapPlot <- NULL
+})
+
+# Process the uploaded file and store the data
+observeEvent(input$dataFile, {
+  # Check if a file is uploaded
+  if (!is.null(input$dataFile)) {
+    # Read the uploaded file
+    df <- read.xlsx(input$dataFile$datapath)
+    
+    # Store the data in the reactive value
+    uploaded_data(df)
+  }
+})
+
+options(shiny.maxRequestSize = 50*1024^2)  # Set maximum file size to 50MB
 
 # Run the application
 shinyApp(ui, server)
-
